7a2212525c41c6a9bb1ee07f1bb1283aa19e3438
Sandoval-Jordan <david.molinaj2018@gmail.com>
2025-11-10 15:32:21 -0300

    archivo jflap

--- probando.py
+++ probando.py
@@ -1,1 +1,11 @@
+import authlib;
+
 print("Hello world!")
+
+farmacia
+terminal -> turnos
+3 trunos case
+PERFUMERIA -> P 01 -> SE ATIENDE EN X
+SIN RECETA -> S 01 -> SE ATIENDE EN Y
+CON RECETA -> C 01  ->SE ATIENDE EN Z  REQUISITO : OBRA SOCIAL, RECETA MEDICA DEL MEDICO
+
